981e78ac905f547147215145f8fa0aca757280a5
lucionexus <bossreyshan@gmail.com>
2022-04-21 14:58:45 +0700

    menambah data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import MovieCard from "./MovieCard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movieList: [],
+      isLoading: false,
+    };
+  }
+
+  fetchMovieList() {
+    fetch("https://api.jikan.moe/v3/top/anime")
+      .then((response) => response.json())
+      .then((result) => {
+        this.setState({ movieList: result.top });
+      })
+      .catch((error) => error);
+  }
+
+  componentDidMount() {
+    this.fetchMovieList();
+  }
+
+  componentDidUpdate() {
+    if (this.state.movieList.length != 0 && !this.state.isLoading) {
+      this.setState({ isLoading: true });
+    }
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return (
+        <div className="container my-5">
+          <h1 className="text-center">Anime List</h1>
+          <div className="container my-5">
+            <div id="daftar-anime" className="row">
+              {this.state.movieList.map((anime) => {
+                return (
+                  <div className="col-md my-3" key={anime.mal_id}>
+                    <MovieCard movie={anime} />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return <h2>Loading ...</h2>;
+    }
+  }
 }
 
 export default App;
